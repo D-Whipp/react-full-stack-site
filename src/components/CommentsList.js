c41709661a83c9eed57d5485dcd082982fa5b015
David Whipple <dwhipp88@gmail.com>
2022-10-17 19:02:45 -0600

    commentsList component does not display, I am working through the code to locate the error

--- src/components/CommentsList.js
+++ src/components/CommentsList.js
@@ -1,10 +1,10 @@
 const CommentsList = ({ comments }) => {
   <>
-    <h3>Comments: </h3>
+    <h3>Comments:</h3>
     {comments.map((comment) => (
       <div
         className="comment"
-        key={comment.postedBY + ':' + comment.text}
+        key={comment.postedBY + ': ' + comment.text}
       >
         <h4>{comment.postedBy}</h4>
         <p>{comment.text}</p>
--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 import articles from './article-content';
 
 const ArticlePage = () => {
@@ -25,7 +26,7 @@ const ArticlePage = () => {
   const article = articles.find(
     (article) => article.name === articleId
   );
-
+ 
   const addUpvote = async () => {
     const response = await axios.put(
       `/api/articles/${articleId}/upvote`
@@ -57,6 +58,12 @@ const ArticlePage = () => {
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
+      <AddCommentForm
+        articleName={articleId}
+        onArticleUpdated={(updatedArticle) =>
+          setArticleInfo(updatedArticle)
+        }
+      />
       <CommentsList comments={articleInfo.comments} />
     </>
   );
